5391de72f0d27b150351717ae3e8d4e22cf6952d
flavienbwk <flavien@berwick.fr>
2021-03-25 23:17:35 +0100

    Simplifying dst path and adding CI

--- mirror.sh
+++ mirror.sh
@@ -9,7 +9,7 @@ dest=/downloads
 for arch in "x86_64"
 do
 	arch_src="$src/${version}/$arch"
-	arch_dest="$dest/${version}"
+	arch_dest="$dest"
 	mkdir -p "$arch_dest"
 	wget \
 		--mirror -nH --no-check-certificate --cut-dirs=1 --convert-links --adjust-extension --page-requisites --no-parent \
